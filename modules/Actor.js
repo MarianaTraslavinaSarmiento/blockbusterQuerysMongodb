d518c129534dc45fdef4e8eae14a0134d2ac7219
MarianaTraslavinaSarmiento <156379477+MarianaTraslavinaSarmiento@users.noreply.github.com>
2024-07-05 16:57:03 -0500

    feat: :sparkles: Querys completed

--- modules/Actor.js
+++ modules/Actor.js
@@ -1,133 +1,142 @@
 import { ObjectId } from "mongodb";
-import { connect } from "../helpers/db/connect.js"
-
+import { connect } from "../helpers/db/connect.js";
 
 export class Actor extends connect {
-    static instanceActor;
-    db
-    constructor() {
-        super();
-        this.db = this.conexion.db(this.getDbName);
-        this.collection = this.db.collection('actor');
-        if (typeof Actor.instance === 'object') {
-            return Actor.instance;
-        }
-        Actor.instance = this;
-        return this;
+  static instanceActor;
+  db;
+  constructor() {
+    super();
+    this.db = this.conexion.db(this.getDbName);
+    this.collection = this.db.collection("actor");
+    if (typeof Actor.instance === "object") {
+      return Actor.instance;
     }
+    Actor.instance = this;
+    return this;
+  }
 
-    //Encontrar todos los actores que han ganado premios Oscar
-    async getAllActorsWonOscarAward() {
-        await this.conexion.connect()
-        const data = await this.collection.find(
-            { "awards.name": "Oscar Award" }
-        ).project({ "full_name": 1 }).toArray();
-        await this.conexion.close();
-        return data;
-    }
+  //Encontrar todos los actores que han ganado premios Oscar
+  async getAllActorsWonOscarAward() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .find({ "awards.name": "Oscar Award" })
+      .project({ full_name: 1 })
+      .toArray();
+    await this.conexion.close();
+    return data;
+  }
 
-    //Encontrar la cantidad total de premios que ha ganado cada actor
-    async totalAwardsActorWon() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [{ $addFields: { awardQuantity: { $size: '$awards' } } }]
-        ).toArray()
+  //Encontrar la cantidad total de premios que ha ganado cada actor
+  async totalAwardsActorWon() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([{ $addFields: { awardQuantity: { $size: "$awards" } } }])
+      .toArray();
 
-        await this.conexion.close()
-        return data
-    }
+    await this.conexion.close();
+    return data;
+  }
 
-    async getActorsBornAfterDate() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate([
-            {
-                $match: {
-                    'date_of_birth': {
-                        $gt: '1980-12-31'
-                    }
-                }
-            }
-        ]).toArray()
-        await this.conexion.close()
-        return data
-    }
-
-    async actorWhoHaveMoreAwards() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [
-                {
-                    $set: {
-                        'actor_con_mas_premios': {
-                            $size: '$awards'
-                        }
-                    }
-                },
-                { $sort: { 'actor_con_mas_premios': -1 } },
-                { $project: { 'actor_con_mas_premios': 0 } },
-                { $limit: 1 }
-            ]
-        ).toArray()
-        await this.conexion.close()
-        return data
-    }
-
-    async getAllActors() {
-        await this.conexion.connect()
-        const data = await this.collection.countDocuments()
-        await this.conexion.close()
-        return data
-    }
-
-    async getAllActorsAverageAge() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [
-                {
-                    $addFields: {
-                        edad: {
-                            $dateDiff: {
-                                startDate: {
-                                    $dateFromString: {
-                                        dateString: "$date_of_birth",
-                                    },
-                                },
-                                endDate: new Date(),
-                                unit: "year",
-                            },
-                        },
-                    },
-                },
-                {
-                    $group: {
-                        _id: null,
-                        promedioEdad: { $avg: "$edad" },
-                    },
-                },
-            ]
-        ).toArray()
-        await this.conexion.close()
-        return data
-    }
-
-    async getAllActorsHaveInstagram() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate([
-            { $unwind: "$social_media" },
-
-            {
-                $project: {
-                    _id: 0,
-                    full_name: 1,
-                    instagram: { exists: true },
-                },
+  async getActorsBornAfterDate() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        {
+          $match: {
+            date_of_birth: {
+              $gt: "1980-12-31",
             },
-        ]).toArray()
-        await this.conexion.close()
-        return data
-    }
+          },
+        },
+      ])
+      .toArray();
+    await this.conexion.close();
+    return data;
+  }
 
-    
+  async actorWhoHaveMoreAwards() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        {
+          $set: {
+            actor_con_mas_premios: {
+              $size: "$awards",
+            },
+          },
+        },
+        { $sort: { actor_con_mas_premios: -1 } },
+        { $project: { actor_con_mas_premios: 0 } },
+        { $limit: 1 },
+      ])
+      .toArray();
+    await this.conexion.close();
+    return data;
+  }
 
+  async getAllActors() {
+    await this.conexion.connect();
+    const data = await this.collection.countDocuments();
+    await this.conexion.close();
+    return data;
+  }
 
+  async getAllActorsAverageAge() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        {
+          $addFields: {
+            edad: {
+              $dateDiff: {
+                startDate: {
+                  $dateFromString: {
+                    dateString: "$date_of_birth",
+                  },
+                },
+                endDate: new Date(),
+                unit: "year",
+              },
+            },
+          },
+        },
+        {
+          $group: {
+            _id: null,
+            promedioEdad: { $avg: "$edad" },
+          },
+        },
+      ])
+      .toArray();
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllActorsHaveInstagram() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        { $unwind: "$social_media" },
+
+        {
+          $project: {
+            _id: 0,
+            full_name: 1,
+            instagram: { exists: true },
+          },
+        },
+      ])
+      .toArray();
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllActorsWonAwardsAfterAYear({ year } = { year: 2015 }) {
+    await this.conexion.connect();
+    const data = await this.collection.find({"awards.year":{$gt: year}}).project({full_name: 1}).toArray();
+    await this.conexion.close();
+    return data;
+  }
+
+  
 }
--- modules/Movie.js
+++ modules/Movie.js
@@ -1,159 +1,247 @@
-
-
 import { ObjectId } from "mongodb";
-import { connect } from "../helpers/db/connect.js"
+import { connect } from "../helpers/db/connect.js";
 
+export class Movie extends connect {
+  //hereda las propiedades de la clase connect
+  static instanceMovie; //Asegura que solo haya una instancia de la clase
+  db; //Almacena la referencia a la base de datos obtenida desde la conexion establecida en connect
+  collection;
 
-export class Movie extends connect { //hereda las propiedades de la clase connect
-    static instanceMovie; //Asegura que solo haya una instancia de la clase
-    db //Almacena la referencia a la base de datos obtenida desde la conexion establecida en connect
-    collection
-
-    constructor() {
-        super(); //llama al constructor de la clase padre (connect) inicializando la conexion a la bd usando los valores ya establecidos
-        this.db = this.conexion.db(this.getDbName);
-        this.collection = this.db.collection('movie'); //asigna la base de datos actual (osea this.db) usando el metodo db() de la conexion establecida en connect, usando el nombre de al base de datos obtenido atraves del getter getdbName
-        if (Movie.instanceMovie) {
-            Movie.instanceMovie;
-        }
-        Movie.instanceMovie = this; //Si no existe la instancia, se establece como this, (la instancia actual de movie)
+  constructor() {
+    super(); //llama al constructor de la clase padre (connect) inicializando la conexion a la bd usando los valores ya establecidos
+    this.db = this.conexion.db(this.getDbName);
+    this.collection = this.db.collection("movie"); //asigna la base de datos actual (osea this.db) usando el metodo db() de la conexion establecida en connect, usando el nombre de al base de datos obtenido atraves del getter getdbName
+    if (Movie.instanceMovie) {
+      Movie.instanceMovie;
     }
+    Movie.instanceMovie = this; //Si no existe la instancia, se establece como this, (la instancia actual de movie)
+  }
 
-    //Contar el número total de copias de DVD disponibles en todos los registros
-    async getAllDVDCopies() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [
-                { $unwind: '$format' },
-                { $match: { 'format.name': 'dvd' } },
-                {
-                    $group: {
-                        _id: null,
-                        total_copias_dvd: { $sum: "$format.copies" }
-                    }
-                }
-            ]
-        ).toArray();
-        await this.conexion.close();
-        return data;
-    }
+  //Contar el número total de copias de DVD disponibles en todos los registros
+  async getAllDVDCopies() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        { $unwind: "$format" },
+        { $match: { "format.name": "dvd" } },
+        {
+          $group: {
+            _id: null,
+            total_copias_dvd: { $sum: "$format.copies" },
+          },
+        },
+      ])
+      .toArray();
+    await this.conexion.close();
+    return data;
+  }
 
-    async getAllDistinctGenreOfMovies() {
-        await this.conexion.connect()
-        const data = await this.collection.distinct('genre')
-        await this.conexion.close()
-        return data
-    }
+  async getAllDistinctGenreOfMovies() {
+    await this.conexion.connect();
+    const data = await this.collection.distinct("genre");
+    await this.conexion.close();
+    return data;
+  }
 
-    async getAllMoviesActorOnePerformed({ id } = { id: 1 }) {
-        await this.conexion.connect()
-        const data = this.collection.aggregate([
-            { $unwind: '$character' },
-            { $match: { "character.id_actor": id } }
-        ]).toArray()
-        return data
-    }
+  async getAllMoviesActorOnePerformed({ id } = { id: 1 }) {
+    await this.conexion.connect();
+    const data = this.collection.find({"character.id_actor":id})
+    .project({"name":true})
+    .toArray();
+    return data;
+  }
 
-    async getAllAvailableDVDCopies({ format } = { format: 'dvd' }) {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate([
-            { $unwind: '$format' },
-            { $match: { 'format.name': format } },
-            {
-                $group: {
-                    _id: null,
-                    total_valor: { $sum: { $multiply: ['$format.value', '$format.copies'] } }
-                }
+  async getAllAvailableDVDCopies({ format } = { format: "dvd" }) {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        { $unwind: "$format" },
+        { $match: { "format.name": format } },
+        {
+          $group: {
+            _id: null,
+            total_valor: {
+              $sum: { $multiply: ["$format.value", "$format.copies"] },
+            },
+          },
+        },
+      ])
+      .toArray();
+
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllMoviesWhereJhonPoePerformed({ actor } = { actor: "John Doe" }) {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        { $unwind: "$character" },
+        {
+          $lookup: {
+            from: "actor",
+            localField: "character.id_actor",
+            foreignField: "id_actor",
+            as: "character.id_actor",
+          },
+        },
+        { $unwind: "$character.id_actor" },
+        {
+          $match: { "character.id_actor.full_name": actor },
+        },
+      ])
+      .toArray();
+
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllMoviesWherePrincipalsPerformed({ rol } = { rol: "principal" }) {
+    await this.conexion.connect();
+    const data = await this.collection.find({"character.rol": rol})
+    .toArray();
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllAwardsOfTheMovies() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        { $unwind: "$character" },
+        {
+          $lookup: {
+            from: "actor",
+            localField: "character.id_actor",
+            foreignField: "id_actor",
+            as: "actor_info",
+          },
+        },
+
+        { $unwind: "$actor_info" },
+
+        {
+          $set: {
+            premios: { $size: "$actor_info.awards" },
+          },
+        },
+
+        {
+          $group: {
+            _id: null,
+            total_premios: { $sum: "$premios" },
+          },
+        },
+      ])
+      .toArray();
+
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllBlurayMoviesJohnDoePerformedAnd() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        { $unwind: "$character" },
+        {
+          $lookup: {
+            from: "actor",
+            localField: "character.id_actor",
+            foreignField: "id_actor",
+            as: "character.id_actor",
+          },
+        },
+        { $unwind: "$character.id_actor" },
+        {
+          $match: {
+            "character.id_actor.full_name": "John Doe",
+            "format.name": "Bluray",
+          },
+        },
+        {
+          $project: {
+            name: 1,
+          },
+        },
+      ])
+      .toArray();
+
+    await this.conexion.close();
+    return data;
+  }
+
+  async getAllSciFiMoviesWhereActorOnePerformed() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        {
+          $match: {
+            $and: [{ genre: "Ciencia Ficción" }, { "character.id_actor": 3 }],
+          },
+        },
+      ])
+      .toArray();
+
+    await this.conexion.close();
+    return data;
+  }
+
+  async getDVDMovieThatHaveMoreCopies() {
+    await this.conexion.connect();
+    const data = await this.collection
+      .aggregate([
+        {
+          $unwind: "$format",
+        },
+        {
+          $match: {
+            "format.name": "dvd",
+          },
+        },
+        {
+          $sort: {
+            "format.copies": -1,
+          },
+        },
+        {
+          $limit: 1,
+        },
+      ])
+      .toArray();
+
+    await this.conexion.close();
+    return data;
+  }
+
+  async getTotalValueOfBlurayMovies(){
+    await this.conexion.connect()
+    const data = await this.collection.aggregate([
+        {
+          $unwind: "$format"
+        },
+        {
+          $match: {
+            "format.name":'Bluray'
+          }
+        },
+        {
+          $set: {
+            total_valor_copias: {$multiply: ["$format.value","$format.copies"]}
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            count: {
+              $sum: "$total_valor_copias"
             }
-        ]).toArray()
+          }
+        }
+      ]).toArray()
 
-        await this.conexion.close()
-        return data
-    }
-
-    async getAllMoviesWhereJhonPoePerformed({ actor } = { actor: 'John Doe' }) {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [
-                { $unwind: '$character' },
-                {
-                    $lookup: {
-                        from: 'actor',
-                        localField: "character.id_actor",
-                        foreignField: 'id_actor',
-                        as: "character.id_actor"
-                    }
-                },
-                { $unwind: '$character.id_actor' },
-                {
-                    $match: { 'character.id_actor.full_name': actor }
-                }
-            ]
-        ).toArray()
-
-        await this.conexion.close()
-        return data
-    }
-
-    async getAllMoviesWherePrincipalsPerformed({ rol } = { rol: 'principal' }) {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [
-                {
-                    $match: {
-                        'character.rol': { $eq: rol }
-                    }
-                }
-            ]
-        ).toArray()
-
-        await this.conexion.close()
-        return data
-    }
-
-    async getAllAwardsOfTheMovies() {
-        await this.conexion.connect()
-        const data = await this.collection.aggregate(
-            [
-                { $unwind: "$character" },
-                {
-                    $lookup: {
-                        from: "actor",
-                        localField: "character.id_actor",
-                        foreignField: "id_actor",
-                        as: "actor_info",
-                    },
-                },
-
-                { $unwind: "$actor_info" },
-
-                {
-                    $set: {
-                        premios: { $size: "$actor_info.awards" },
-                    },
-                },
-
-                {
-                    $group: {
-                        _id: null,
-                        total_premios: { $sum: "$premios" },
-                    },
-                },
-            ]
-        ).toArray()
-
-        await this.conexion.close()
-        return data
-    }
-
- 
-
-
-
-
-
-
-
+      await this.conexion.close()
+      return data
+  }
 
 }
-
